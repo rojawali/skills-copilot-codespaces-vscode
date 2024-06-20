51767a98e90349b7b2eda464dd94f6a995d2b50f
rojawali <139275852+rojawali@users.noreply.github.com>
2024-06-20 00:35:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+  return {
+    members: [
+      { name: 'John', skills: ['JavaScript', 'React'] },
+      { name: 'Jane', skills: ['JavaScript', 'Angular'] },
+    ],
+  };
+}
